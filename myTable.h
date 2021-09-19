53f31899992205a261e3f37a8d4534e9fbc93884
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-09-19 20:49:04 +0200

    Update myTable.h

--- myTable.h
+++ myTable.h
@@ -8,29 +8,34 @@ class myTable {
 private:
 	int row;
 	int column;
-	std::vector<std::vector<int>> Cells;
+	std::vector<std::vector<std::string>> Cells;
+	static const std::string columnLetters;
+	static const std::string emptyCellSymbol;
 
 public:
-	std::vector<std::vector<int>>* ptrToCells;
+	std::vector<std::vector<std::string>>* ptrToCells;
 	int getRow() const{ return row; }
 	int getColumn() const{ return column; }
 	void setRow(int toSet) { row = toSet; }
 	void setColumn(int toSet) { column = toSet; }
-	void setCell(int yPos, int xPos, int toSet) { Cells[xPos][yPos] = 1; } //toSet "kikapcsolva" , minden setCell 1re állítja a választott cellát
+	void setCell(int yPos, int xPos, std::string const& toSet);
 	
-	//DESTRUKTOR / Memória kezelés hiányzik
+	//DESTRUKTOR / MemÃ³ria kezelÃ©s hiÃ¡nyzik
 
 	void addRows(int);
 	void addColumns(int);
-	void resizeFirstRow(int);
 
-	void printTableSpecs() { std::cout << "Rows: " << row << " Columns: " << column << std::endl; }
+	void deleteRow(int);
+	void deleteColumn(int);
+
+	void printTableSpecs() { std::cout << "Rows: " << Cells[0].size() << " Columns: " << Cells.size() << std::endl; 
+							 std::cout << "Rows: " << getRow() << " Columns: " << getColumn() << std::endl;}
 	void printTable() const;
 	void printCell(int xPos, int yPos) const{ std::cout << Cells[xPos][yPos] << " "; }
 
 	myTable() : row(1), column(1), ptrToCells(&Cells) {
-		Cells.push_back(std::vector<int>());
-		Cells[0].push_back(0);
+		Cells.push_back(std::vector<std::string>());
+		Cells[0].push_back(emptyCellSymbol);
 	}
 
 };
