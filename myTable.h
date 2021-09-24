313a583e7b27bd7ddda56e4e6990ef0f6979d0cb
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-09-24 19:23:37 +0200

    Update myTable.h

--- myTable.h
+++ myTable.h
@@ -4,6 +4,19 @@
 #include<vector>
 #include<iostream>
 
+/*
+	  ------- SOR / ROW----->  Cells[i].push_back(var);
+	|					  Cells[i].resize(size, var);
+	|
+	|
+  OSZLOP
+  COLUMN        Cells.push_back(vector<int>());
+	|		    Cells.resize(size, vector<int>(0));
+	|
+	↓
+
+*/
+
 class myTable {
 private:
 	int row;
@@ -13,14 +26,14 @@ private:
 	static const std::string emptyCellSymbol;
 
 public:
-	std::vector<std::vector<std::string>>* ptrToCells;
-	int getRow() const{ return row; }
-	int getColumn() const{ return column; }
+	//std::vector<std::vector<std::string>>* ptrToCells;   // for future use
+	int getRow() const { return row; }
+	int getColumn() const { return column; }
 	void setRow(int toSet) { row = toSet; }
 	void setColumn(int toSet) { column = toSet; }
 	void setCell(int yPos, int xPos, std::string const& toSet);
-	
-	//DESTRUKTOR / Memória kezelés hiányzik
+
+	//DESTRUKTOR missing/ Memória kezelés hiányzik maybe
 
 	void addRows(int);
 	void addColumns(int);
@@ -28,12 +41,14 @@ public:
 	void deleteRow(int);
 	void deleteColumn(int);
 
-	void printTableSpecs() { std::cout << "Rows: " << Cells[0].size() << " Columns: " << Cells.size() << std::endl; 
-							 std::cout << "Rows: " << getRow() << " Columns: " << getColumn() << std::endl;}
+	void printTableSpecs() {
+		std::cout << "Rows: " << Cells[0].size() << " Columns: " << Cells.size() << std::endl;
+		std::cout << "Rows: " << getRow() << " Columns: " << getColumn() << std::endl;
+	}
 	void printTable() const;
-	void printCell(int xPos, int yPos) const{ std::cout << Cells[xPos][yPos] << " "; }
+	void printCell(int xPos, int yPos) const { std::cout << Cells[xPos][yPos] << " "; }
 
-	myTable() : row(1), column(1), ptrToCells(&Cells) {
+	myTable() : row(1), column(1) {   //Default Constructor
 		Cells.push_back(std::vector<std::string>());
 		Cells[0].push_back(emptyCellSymbol);
 	}
@@ -41,4 +56,3 @@ public:
 };
 
 #endif // !MYTABLE_H
-
