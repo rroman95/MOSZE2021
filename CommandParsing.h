699bad6a55b4439ba5afdb9f47e8b7bc880a49e0
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-09-24 19:22:34 +0200

    added Commands class

--- CommandParsing.h
+++ CommandParsing.h
@@ -7,7 +7,20 @@
 #include<map>
 #include "myTable.h"
 
-//Forward Declaration
+//DEFINITIONS
+
+class Commands {
+public:
+	std::map<std::string, int> command_codes;		// should be private w/ a getter function, but yolo
+
+	Commands() {
+		command_codes.insert(std::pair<std::string, int>("edit", 1));
+		command_codes.insert(std::pair<std::string, int>("delete", 3));
+		command_codes.insert(std::pair<std::string, int>("insert", 4));
+		command_codes.insert(std::pair<std::string, int>("exit", 5));
+		command_codes.insert(std::pair<std::string, int>("add", 7));
+	}
+};
 
 std::string InputTokenization(std::string const& input);
 int findCommandForSwitchStatement(std::string const& command);
