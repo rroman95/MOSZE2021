2e030231dc26fdf8b70fc0952dfb4dbeefb600c9
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-12 20:39:03 +0200

    ignore

--- IOtest.sh
+++ IOtest.sh
@@ -2,8 +2,10 @@
 
 IFS=$'\n'
 
+<<IGNORE
 printf 'edit a1 asd\n' | ./project
 printf 'add 2 rows\n' | ./project
 printf 'add 2 columns\n' | ./project
 printf 'edit c3 end\n' | ./project
+IGNORE
 printf 'exit\n' | ./project
