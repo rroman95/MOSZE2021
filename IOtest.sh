050436c62014e52403c199bbbe6deff171f42e58
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-12 21:54:03 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -3,10 +3,12 @@
 IFS=$'\n'
 
 
-./project input.csv;echo "edit a1 asdasd
+./project input.csv << EOF
+echo "edit a1 asdasd
 add 2 rows
 add 2 columns
 edit c3 LE GRANDE FINALE
 save output.csv
-exit" | ./project
+exit"
+EOF
 
