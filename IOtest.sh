a9aa702a8c98620921a3cf4546b20572351d4333
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-12 21:20:25 +0200

    the CORRECT WAY ??

--- IOtest.sh
+++ IOtest.sh
@@ -3,8 +3,9 @@
 IFS=$'\n'
 
 
-echo "edit a1 asd" | ./project
-echo "add 2 rows" | ./project
-echo "add 2 columns" | ./project
-echo "edit c3 LE GRANDE FINALE" | ./project
-echo "exit" | ./project
+echo "edit a1 asd
+add 2 rows
+add 2 columns
+edit c2 LE GRANDE FINALE
+exit" | ./project
+
