3515b3253fbac2be86cf0d03e2566b57a5697d73
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-13 00:07:58 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -2,12 +2,13 @@
 
 IFS=$'\n'
 
-# ./project input.csv < input.txt   <-- Opens project with input.csv as arg and streamt input.txt as command line inputs
+./project input.csv < input.txt
 
+<<COMMENT
 echo "edit a1 asd
 add 2 rows
 add 2 columns
 edit c2 LE GRANDE FINALE
 save output.csv
 exit" | ./project
-
+COMMENT
