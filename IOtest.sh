a19743078285f835289831a124a266a2d2c923fa
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-12 20:48:30 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -9,5 +9,6 @@ printf 'add 2 rows\n' | ./project
 printf 'add 2 columns\n' | ./project
 printf 'edit c3 end\n' | ./project
 IGNORE
+./project
 echo -ne '\n' | edit a1 asd
 printf 'exit\n' | ./project
