9457b36e8f3386cd70a41aedafdce2a284e27a01
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-12 20:32:50 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -2,6 +2,8 @@
 
 IFS=$'\n'
 
-
-./project >> testoutput.txt
+printf 'edit a1 asd\n' | ./project
+printf 'add 2 rows\n' | ./project
+printf 'add 2 columns\n' | ./project
+printf 'edit c3 end\n' | ./project
 printf 'exit\n' | ./project
