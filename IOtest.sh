60f1baa91db6371530f0ba4b5c1470c2698ac956
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-13 13:58:16 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -5,17 +5,18 @@ IFS=$'\n'
 
 #./project input.csv < input.txt
 
-<<COMMENT
+
 while IFS= read -r LINE; do
    echo "$LINE"
 done < input.txt | ./project
-COMMENT
 
+<<COMMENT
 echo "edit   a1    asd   
 add 2  rows
 add 2 columns
 edit c2 LE GRANDE FINALE
 save output.csv 
 exit" | ./project
+<<
 
 
