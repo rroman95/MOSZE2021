86d98749de46ca91b11e99e6a1ba652848082246
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-13 15:49:35 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -3,18 +3,19 @@
 IFS=$'\n'
 
 
-<<ASD
+
 while IFS='\n' read -r LINE; do
    echo "$LINE"
 done < input.txt | ./project
-ASD
 
+<<ASD
 echo "edit   a1    asd   
 add 2  rows
 add 2 columns
 edit c2 LE GRANDE FINALE   
 save output.csv   
 exit      " | ./project
+ASD
 
 
 #./project input.csv < input.txt
