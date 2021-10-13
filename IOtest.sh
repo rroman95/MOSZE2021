7a080e8d446f04d4af16a2027768d6176a27b225
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-13 13:28:40 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -3,8 +3,9 @@
 IFS=$'\n'
 
 
-#./project input.csv < input.txt
+./project input.csv < input.txt
 
+<<COMMENT
 declare -a ARRAY
 while IFS= read -r line; do
     echo "Text read from file: $line"
@@ -18,12 +19,13 @@ for i in ${ARRAY[@]}; do
 echo ${ARRAY[i]};
 echo "---"
 done
+COMMENT
 
-<<COMMENT
+<<COMMENT2
 echo "edit   a1    asd   
 add 2  rows
 add 2 columns
 edit c2 LE GRANDE FINALE
 save output.csv 
 exit" | ./project
-COMMENT
+COMMENT2
