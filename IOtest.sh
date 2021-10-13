abe4c6ac7656d70b347399c4a5be454ab12576c3
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-13 12:59:36 +0200

    Update IOtest.sh

--- IOtest.sh
+++ IOtest.sh
@@ -14,7 +14,7 @@ echo "Array elements:"
 
 #echo ${#ARRAY[@]}
 
-for i in ${ARRAY[@]}; do printf $i | ./project done 
+for i in ${ARRAY[@]}; do printf $i | ./project; done 
 
 <<COMMENT
 echo "edit   a1    asd   
